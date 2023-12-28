9537303f5acc81a380348d1a85c3259dc260ccde
HC4X <faeldojo@gmail.com>
2023-12-28 15:16:39 -0300

    feat: eventos

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,18 +1,19 @@
 <template>
     <div>
         <h2>Meu formulário</h2>
-    <form action="">
+    <form action="" @submit="enviarFormulario($event)">
         <div>
-            <InputText />
+            <input type="text" v-model="name">
         </div>
         <div>
-            <InputText />
+            <input type="text" v-model="email">
         </div>
         <div>
             <Submit />
         </div>
         
     </form>
+    <p>Conteúdo</p>
     </div>
 </template>
 <script>
@@ -23,6 +24,32 @@ export default {
     components: {
         InputText,
         Submit
+    },
+    data() {
+        return {
+            name: "",
+            email:""
+        }
+    },
+    methods:{
+        enviarFormulario(e){
+
+            const name = this.name;
+            const email = this.email;
+
+            e.preventDefault();
+            console.log("Formulário enviado !")
+            console.log("O nome é " + name)
+            console.log("O email é " + email)
+
+            // Requisição para inserir no banco os dados
+
+        }
     }
 }
 </script>
+<style scoped>
+    p{
+        margin: 150px;
+    }
+</style>
