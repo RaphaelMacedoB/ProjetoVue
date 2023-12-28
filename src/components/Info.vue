077c6f150016ab6b7575db8aef4858639ab996bf
HC4X <faeldojo@gmail.com>
2023-12-28 14:36:29 -0300

    feat: atributos dinamicos

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -9,16 +9,24 @@
             <li>Python</li>
         </ul>
         <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
+        <p>Para acessar meu portifólio <a v-bind:href="meu_link" target="_blank">basta clicar aqui</a></p>
+        <Picture />
     </div>
 </template>
 <script>
+import Picture from './Picture.vue';
+
 export default {
     name: "Info",
+    components:{
+        Picture
+    },
     data() {
-        return {
+            return {
             esta_trabalhando: false,
             mostrar_email: true,
             email: "faeldojo@gmail.com",
+            meu_link: "https://google.com"
         }
     },
 }
