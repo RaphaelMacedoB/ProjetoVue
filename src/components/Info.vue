2da6b502a272c37c27d20fe44be7b1a9f3457c05
HC4X <faeldojo@gmail.com>
2023-12-28 15:06:07 -0300

    feat: list render using v-for

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -2,11 +2,15 @@
     <div>
         <p v-if="esta_trabalhando">Estou trabalhando no momento</p>
         <p v-else>Não estou trabalhando</p>   
-        <p>Utilizo as seguintes tecnologias</p>
+        <p>Utilizo as seguintes tecnologias para back-end:</p>
         <ul>
-            <li>JavaScript</li>
-            <li>PHP</li>
-            <li>Python</li>
+            <li v-for="(technology,index) in backend_technologies" :key="index">{{ technology }}</li>
+        </ul>
+        <p>Utilizo as seguintes tecnologias para front-end: </p>
+        <ul>
+            <li v-for="technology in frontend_tecnologies" :key="technology.id">
+            {{ technology.language }}
+            </li>
         </ul>
         <div>
             <button @click="showEmail">{{ textoBotao }}</button>
@@ -30,7 +34,13 @@ export default {
             mostrar_email: false,
             email: "faeldojo@gmail.com",
             meu_link: "https://google.com",
-            textoBotao: "Mostrar e-mail"
+            textoBotao: "Mostrar e-mail",
+            backend_technologies: ["JavaScript","PHP","Python"],
+            frontend_tecnologies: [
+                {id: 1, language: "HTML"},
+                {id:2 , language: "CSS"},
+                {id:3, language: "Vue"}
+            ]
         }
     },
     methods:{
